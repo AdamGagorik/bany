5c57173d2f948314c57a7ff562d0ede179898998
Adam Gagorik <AdamGagorik@users.noreply.github.com>
2023-01-03 21:50:06 -0500

    Fix logger name and format (#8)

--- bany/core/logger.py
+++ bany/core/logger.py
@@ -3,8 +3,8 @@ import logging
 from rich.console import Console
 
 
-logger = logging.getLogger("pdf2ynab")
-console = Console(force_terminal=True, width=1024)
+logger = logging.getLogger("bany")
+console = Console()
 
 
 def logline(level=logging.INFO, c="-", w=120):
